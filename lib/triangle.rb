280e43d62f4551473c1b1dcbb77003fbe3192b7c
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 20:19:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,18 @@
 class Triangle
   # write code here
   attr_accessor :length1, :length2, :length3
+  attr_reader :type
   def initialize(length1:, length2:, length3:)
-    
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
   end
+
+  def kind
+    if self.length1 == self.length2 == self.length3
+      
+  end
+  
 end
 
 
