3a679b1e7c0d8fc9e20a5b24f3baf583c638985c
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 20:48:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,16 +22,15 @@ class Triangle
 
   def illegal?
     if self.x <= 0 || self.y <= 0 || self.z <= 0
-
+    elsif
+      self.x + self.y < self.z || self.y + self.z < self.x || self.x + self.z < self.y
     end
   end
 
   def kind
     if illegal?
-      begin
         raise TriangleError
-
-      end
+        puts error.message
     elsif
       equilateral?
       @type = :equilateral
