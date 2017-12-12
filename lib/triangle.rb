8d12c1fca9e2b5da4c4ea120e6568020b87f7067
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 20:33:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   # write code here
   attr_accessor :length1, :length2, :length3, :type
-  def initialize(length1:, length2:, length3:)
+  def initialize(length1, length2, length3)
     @length1 = length1
     @length2 = length2
     @length3 = length3
